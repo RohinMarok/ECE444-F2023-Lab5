6b59d10277ff3adef24f1a015f3349011f78089c
scientlogistics <rohin@scientsolutions.ca>
2023-10-27 19:25:14 -0400

    done all code no errors

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -57,20 +57,6 @@ def add_entry():
     return redirect(url_for("index"))
 
 
-@app.route("/delete/<int:post_id>", methods=["GET"])
-@login_required
-def delete_entry(post_id):
-    """Deletes post from database."""
-    result = {"status": 0, "message": "Error"}
-    try:
-        new_id = post_id
-        db.session.query(models.Post).filter_by(id=new_id).delete()
-        db.session.commit()
-        result = {"status": 1, "message": "Post Deleted"}
-        flash("The entry was deleted.")
-    except Exception as e:
-        result = {"status": 0, "message": repr(e)}
-    return jsonify(result)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -96,21 +82,6 @@ def logout():
     flash("You were logged out")
     return redirect(url_for("index"))
 
-
-@app.route("/delete/<int:post_id>", methods=["GET"])
-def delete_entry(post_id):
-    """Deletes post from database."""
-    result = {"status": 0, "message": "Error"}
-    try:
-        db.session.query(models.Post).filter_by(id=post_id).delete()
-        db.session.commit()
-        result = {"status": 1, "message": "Post Deleted"}
-        flash("The entry was deleted.")
-    except Exception as e:
-        result = {"status": 0, "message": repr(e)}
-    return jsonify(result)
-
-
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -120,6 +91,21 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+@app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
+def delete_entry(post_id):
+    """Deletes post from database."""
+    result = {'status': 0, 'message': 'Error'}
+    try:
+        new_id = post_id
+        db.session.query(models.Post).filter_by(id=new_id).delete()
+        db.session.commit()
+        result = {'status': 1, 'message': "Post Deleted"}
+        flash('The entry was deleted.')
+    except Exception as e:
+        result = {'status': 0, 'message': repr(e)}
+    return jsonify(result)
 
 
 @app.route("/search/", methods=["GET"])
